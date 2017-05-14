7db2e5132dd7379d69363b4917cdcc0aa37d876a
Rommel Dominguez <ynwodsun000@outlook.com>
2017-05-14 13:37:32 +0000

    configured for heroku

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var pagpapahayag = require('express');
-var m = require('morgan');
-
+var b = require('body-parser');
+var c = require('cors');
+var u = require('express-useragent');
+// using seemingly unintelligible template to discourage cut and paste culture
 var ap = "/api/whoami";
 
 var a = module.exports = pagpapahayag();
-a.use(m('dev'));
+
+a.use(b.json());
+a.use(c());
+a.use(u.express());
 
 a.get(ap, function(pakiusap, tugon){
-    var wika = pakiusap.headers['accept-language'];
-    var aninag = pakiusap.headers['user-agent'];
-    var proto = pakiusap.headers['x-forwarded-for'];
-    tugon.json({
-        language: wika,
-        software: aninag,
-        ipaddress: proto
+//        console.log(tugon);    
+     var wika = pakiusap.acceptsLanguages();
+     var aninag = "os: " + pakiusap.useragent.os + ", browser: " + pakiusap.useragent.browser;
+     var proto = pakiusap.ip;
+            tugon.json({
+             ipaddress: proto,
+             language: wika[0],
+             software: aninag
+             
     });
 });
 
