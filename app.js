e2d29f1ea3c7bfdcf36c07edb8b79bc1f338040d
Rommel Dominguez <ynwodsun000@outlook.com>
2017-05-15 07:29:30 +0000

    made some changes to the code

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ var pagpapahayag = require('express');
 var b = require('body-parser');
 var c = require('cors');
 var u = require('express-useragent');
+var m = require('morgan');
 // using seemingly unintelligible template to discourage cut and paste culture
 var ap = "/api/whoami";
 
-var a = module.exports = pagpapahayag();
+//var a = module.exports = pagpapahayag();
+var a = pagpapahayag.createServer = pagpapahayag();
 
 a.use(b.json());
 a.use(c());
 a.use(u.express());
 
+//a.get('/', function(pakiusap, tugon, susunod)){
+//        tugon.sendFile(__dirname + '/public/index.html'));
+//});
+a.set("view options", {layout: false});
+a.use(pagpapahayag.static(__dirname + '/public'));
+a.get('/', function(pakiusap, tugon){
+    tugon.render('index.html');
+});
+
+
 a.get(ap, function(pakiusap, tugon){
-//        console.log(tugon);    
+       console.log(tugon);    
      var wika = pakiusap.acceptsLanguages();
      var aninag = "os: " + pakiusap.useragent.os + ", browser: " + pakiusap.useragent.browser;
-     var proto = pakiusap.ip;
+     var proto = pakiusap.headers['x-forwarded-for'];
             tugon.json({
              ipaddress: proto,
              language: wika[0],
